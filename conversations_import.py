956fa7c187b3d3f920e3085bb592215b43d8103b
Subhasmita <subhasmitaswain232@gmail.com>
2024-01-17 20:32:08 +0530

    convert the json date to datetime format

--- conversations_import.py
+++ conversations_import.py
@@ -2,16 +2,21 @@ import boto3
 import csv
 import requests
 import pandas as pd
+import io 
+import datetime
+from dotenv import load_dotenv
+import os
 
-# Intercom API Credentials 
-INTERCOM_API_KEY = 'dG9rOmQyOGUxNzA2X2ZkMTBfNGM2Ml9iOTM4X2FmOWYxZjZlYWQwNToxOjA='
-INTERCOM_API_ENDPOINT= 'https://api.intercom.io/'
+load_dotenv()
 
-# CIVO S3 Credentials
-ACCESS_KEY = 'N0DEU6BRNKJX5XY4SDW6'
-SECRET_KEY = 'r1Hg3ezGKOCmrBhYGpRpOI069cdDd4ggVJ51zzg1'
-ENDPOINT = 'https://objectstore.lon1.civo.com/civo-bi-upload'
-S3_BUCKET_NAME ='experiment'
+# API & S3 Credentials from .env file
+
+INTERCOM_API_KEY = os.getenv('INTERCOM_API_KEY')
+INTERCOM_API_ENDPOINT = os.getenv('INTERCOM_API_ENDPOINT')
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+ENDPOINT = os.getenv('ENDPOINT')
+S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
 s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, endpoint_url=ENDPOINT)
 local_path = 'data'
@@ -35,30 +40,50 @@ def get_intercom_conversations():
             }
     i=0
     conversations = []
-    while i<3:
+    while True:
         response = requests.get(url, headers=headers, params=query)
         print(response.json().get('pages', {}))
         if response.status_code!= 200:
             # print the error response from the api call
             print(f'Error getting conversations {response.status_code} {response.text}')
             return pd.DataFrame()
+        # conversations = response.json().get('conversations',[])
         
+        if i == 1:
+            break
         query['starting_after'] = response.json().get('pages',{}).get('next', {}).get('starting_after', '')
         conversations = conversations + response.json().get('conversations',[])
+
+        if not response.json().get('pages',{}).get('next', {}).get('starting_after', ''):
+            break
         i+=1
 
+
+    # convert the json date columns to datetime
+    for conversation in conversations:
+        conversation['created_at'] = datetime.datetime.utcfromtimestamp(conversation['created_at']).strftime('%Y-%m-%d %H:%M:%S')
+        conversation['updated_at'] = datetime.datetime.utcfromtimestamp(conversation['updated_at']).strftime('%Y-%m-%d %H:%M:%S')
+        conversation['first_contact_reply']['created_at'] = datetime.datetime.utcfromtimestamp(conversation['first_contact_reply']['created_at']).strftime('%Y-%m-%d %H:%M:%S')
+        conversation['statistics']['first_contact_reply_at'] = datetime.datetime.utcfromtimestamp(conversation['statistics']['first_contact_reply_at']).strftime('%Y-%m-%d %H:%M:%S')
+        conversation['statistics']['last_close_at'] = datetime.datetime.utcfromtimestamp(conversation['statistics']['last_close_at']).strftime('%Y-%m-%d %H:%M:%S')
+
+
     normalized_data = pd.json_normalize(conversations)
+
+    # drop the following columns from the data 
+    normalized_data = normalized_data.drop(columns=['type', 'sla_applied', 'source.body', 'source.attachments', 
+                                                    'contacts.contacts', 'tags.type', 'tags.tags', 
+                                                    'teammates.type', 'teammates.admins', 'custom_attributes.Language', 
+                                                    'linked_objects.data', 'linked_objects.total_count', 'linked_objects.has_more', 
+                                                    'conversation_rating.created_at',
+                                                    'conversation_rating.contact.type',
+                                                    'conversation_rating.contact.id',
+                                                    'conversation_rating.contact.external_id',
+                                                    'conversation_rating.teammate.type',
+                                                    'conversation_rating.teammate.id',])
+
     return normalized_data
 
-
-
-def write_to_csv(data, filename):
-    keys = data[0].keys() if data else []
-    with open(filename, 'w', newline='') as csvfile:
-
-        write = csv.DictWriter(csvfile, keys)
-        write.writeheader()
-        write.writerows(data)
 
 # def upload_to_s3(local_file, s3_key):
 #     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, endpoint_url=ENDPOINT)
@@ -66,7 +91,7 @@ def write_to_csv(data, filename):
 
 def upload_to_s3_from_ram(data, filename):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, endpoint_url=ENDPOINT)
-    s3.put_object(Body=data, Bucket=S3_BUCKET_NAME, Key=filename)
+    s3.upload_fileobj(data, Bucket=S3_BUCKET_NAME, Key=filename)
 
 
 def main():
@@ -79,15 +104,16 @@ def main():
 
     
     # filename = f'intercom_{datetime.now().strftime("%Y%m%d%H%M%S")}.csv'
-    # filename = "conversations.csv"
+    filename = "conversation.csv"
 
     # convert to csv and upload to s3 
-    # conversations.to_csv("conversations.csv", index=False)
+    conversations.to_csv("conversations.csv", index=False)
     # upload the file to s3 or replace if there exists a file 
     # upload_to_s3("./conversations.csv", filename)
 
+    files = io.BytesIO(conversations.to_csv(index=False).encode('utf-8'))
     # upload to s3 directly from RAM instead of from disk
-    upload_to_s3_from_ram("conversations.to_csv(conversations.csv, index=False)", "conversations.csv")
+    # upload_to_s3_from_ram(files, filename)
 
     print(f'file uploaded to s3' )
 
